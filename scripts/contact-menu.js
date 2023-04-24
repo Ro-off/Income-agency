8372da0785bd104582b464572e4204b6d55decb2
Romeo <53314439+Ro-off@users.noreply.github.com>
2023-04-24 14:47:19 +0300

    Divide css in different files

--- scripts/contact-menu.js
+++ scripts/contact-menu.js
@@ -1,7 +1,8 @@
 function createListenersForButtons() {
     createListenerForOpenMenu();
     createListenerForCloseMenu();
-    createListenersToOpenNumberLink();
+    //!createListenersToOpenNumberLink();
+    coloringButtonIntoIconColorOnHover();
 }
 
 function createListenerForOpenMenu() {
@@ -24,6 +25,52 @@ function createListenerForCloseMenu() {
     main.addEventListener('click', closeMenu);
 }
 
+function coloringButtonIntoIconColorOnHover() {
+    const buttons = document.querySelectorAll('.container-call-sample');
+
+    const changeColor = (button) => {
+        // console.log(document.querySelector(`#container-call .container-call-sample .${button.firstElementChild.firstElementChild.classList[1]}`).style.backgroundColor);
+        const buttonIcon = button.firstElementChild.firstElementChild;
+      const buttonColor = getComputedStyle(buttonIcon).getPropertyValue('background-color');
+      console.log(buttonColor);
+      
+
+
+      const newColor = lightenColor(buttonColor, 75);
+  button.style.backgroundColor = newColor;
+  button.style.outlineColor = newColor;
+// Устанавливаем новый цвет фона для элемента
+  // buttonIcon.style.backgroundColor = newColor;
+
+function lightenColor(color, percent) {
+  // Удаляем все пробелы из строки и преобразуем строку в массив значений RGB
+  const rgb = color.replace(/\s/g, '').match(/^rgb\((\d+),(\d+),(\d+)\)$/); 
+  // Преобразуем каждое значение RGB в десятичное число
+  const r = parseInt(rgb[1]);
+  const g = parseInt(rgb[2]);
+  const b = parseInt(rgb[3]);
+  // Вычисляем новые значения RGB, увеличивая каждое значение на указанный процент
+  const newR = Math.floor(r + (255 - r) * (percent / 100));
+  const newG = Math.floor(g + (255 - g) * (percent / 100));
+  const newB = Math.floor(b + (255 - b) * (percent / 100));
+  // Возвращаем новый цвет в формате RGB
+  return `rgb(${newR}, ${newG}, ${newB})`;
+}
+
+
+
+
+    }
+    buttons.forEach(button => {
+        button.addEventListener('mouseover', () => {changeColor(button)});
+        button.addEventListener('mouseout', () => {
+          button.style.backgroundColor = 'var(--clear-bg-color)';
+          button.style.outlineColor = 'var(--clear-bg-color)';});
+    });
+
+}
+
+export { createListenersForButtons };
 
  //! Deprecated, will be imbedded in html
 // function createListenersToOpenNumberLink() {
@@ -48,5 +95,3 @@ function createListenerForCloseMenu() {
 // const viberLink = `viber://chat?number=${phoneNumber}`;
 // const telegramLink = `tg://resolve?domain=${phoneNumber}`;
 
-
-export { createListenersForButtons };
